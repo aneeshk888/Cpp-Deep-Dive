7d28ff0a114ac4f63d36bee3e1e54cd26cc940ab
Alienzz <aneeshmk172003@gmail.com>
2026-04-30 08:30:41 +0530

    hello

---
+++ Getting Started With C++/hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello, World!"<<endl; //output the string "Hello, World!" followed by a newline character to the console
+    return 0; //return statement that indicates the end of the main function and returns a value of 0 to the operating system, indicating that the program executed successfully.
+}
